c4f82c0a48731e029df584126d5179f03c43e229
Pedro Ansali <pedrosl1-@hotmail.com>
2023-08-15 14:22:27 -0300

    init do app

---
+++ app-todo/src/app/modules/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItemsComponent } from './components/todo-input-add-items/todo-input-add-items.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItemsComponent,
+    TodoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
